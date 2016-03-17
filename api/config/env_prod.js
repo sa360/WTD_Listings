05df9c1119ac1ec492d9e1f280a038d3b3453c6d
Suzanne Adams <brainstation@BrainStations-MacBook-Air-2.local>
2016-03-17 10:39:14 -0400

    changed env from localhost to AWS IP

--- api/config/env_prod.js
+++ api/config/env_prod.js
@@ -1,3 +1,3 @@
-module.exports = {port:80,host:'http://',database:'production'};
+module.exports = {port:80,host:'http://52.50.174.204/',database:'production'};
 
 // update this with the actual IP address of the instance on AWS
